b063ab599c4bec99e50090aa65e8060b216d3645
Harmi Vaishnav <harmivaishnav2003@gmail.com>
2025-06-11 22:22:08 +0530

    Delete blood_app directory

--- blood_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from blood_app.models import *
-
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Category)
-admin.site.register(Blood_Donation)
-admin.site.register(Order)
